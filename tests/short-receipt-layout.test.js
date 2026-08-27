83a719bb4f8b28129f4b92b1a2007c4d72472932
regener00-creator <regener00@gmail.com>
2026-08-27 12:30:49 +0700

    expand short receipt product names

---
+++ tests/short-receipt-layout.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+const html = fs.readFileSync(path.join(__dirname, '..', 'index.html'), 'utf8');
+const start = html.indexOf('function printShortReceipt(');
+const end = html.indexOf('\nasync function doCheckout', start);
+const receiptCode = html.slice(start, end);
+
+assert.ok(start >= 0 && end > start, 'printShortReceipt must exist');
+assert.match(receiptCode, /grid-template-columns:minmax\(0,1fr\) 19mm/, 'receipt item must reserve only name and amount columns');
+assert.doesNotMatch(receiptCode, /<span>\$\{index\+1\}<\/span>/, 'receipt items must not display a leading sequence number');
+
+console.log('short receipt layout tests passed');
